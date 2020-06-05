a8e9555375c182f86574111bad96e4342c4232f0
YuHan0715 <42367492+YuHan0715@users.noreply.github.com>
2020-06-05 09:47:38 +0800

    TriangleTest

--- src/test/java/TriangleTest.java
+++ src/test/java/TriangleTest.java
@@ -1,3 +1,13 @@
+package com.example;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+@RunWith(JUnit4.class)
+
 public class TriangleTest { 
 	
 	@Test
